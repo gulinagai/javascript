856ae75cc5a4e63610fe87e585cc08517ab942f9
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-11-09 17:49:15 -0300

    exercícios javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 23.4
+console.log(`Sua velocidade é de ${vel}Km/h`)
+if (vel > 70) {
+    console.log(`Sua velocidade está acima do permitido. MULTADO`)
+}
+console.log(`Dirija com segurança!`)
