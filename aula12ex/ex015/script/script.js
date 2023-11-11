521611a9ff943445652ff90309479083cb7b1d4e
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-11-11 15:14:18 -0300

    exercicio 015 javascript resolvido

--- aula12ex/ex015/script/script.js
+++ aula12ex/ex015/script/script.js
@@ -1,17 +1,41 @@
 function verificar() {
-    var ver = window.document.getElementById('msg')
-    //var masc = window.document.getElementById('masc')
-    //var fem = window.document.getElementById('fem')
-
-    var nascstg = window.document.getElementById('nasc')
-    var nascnumberv = Number.(nascstg).value 
+    var cpnasc = window.document.getElementById('nasc')
     var data = new Date()
-    var anoatual = data.getFullYear
-    var idade = anoatual - nascnumberv
-    window.alert(`${idade} anos`)
-    
-
- ver.innerHTML = `Detectamos # com ${idade} anos`
-
-
+    var anoatual = data.getFullYear()
+    var idade = anoatual - Number(cpnasc.value)
+    var opsexo = window.document.getElementsByName('sexo')
+    var msg = window.document.getElementById('msg')
+    var img = window.document.createElement('img')
+     if(cpnasc.value < 1900 || cpnasc.value > anoatual) {
+        msg.innerHTML = `Os dados fornecidos são inválidos`
+    } else if(opsexo[0].checked){
+        var genero = 'Homem'
+    if(idade >= 0 && idade <= 14 && genero == 'Homem') {
+        img.setAttribute('src', 'imagens/homemcrianca.png')
+    } else if(idade > 14 && idade <= 21 && genero == 'Homem') {
+        img.setAttribute('src', 'imagens/homemjovem.png') 
+    } else if(idade > 21 && idade < 60 && genero == 'Homem') {
+        img.setAttribute('src', 'imagens/homemadulto.png')
+    } else if(idade >= 60 && genero == 'Homem') {
+        img.setAttribute('src', 'imagens/homemidoso.png')
+    }
+    } else if (opsexo[1].checked) {
+        var genero = 'Mulher'
+    if(idade >= 0 && idade <= 14 && genero == 'Mulher') {
+        img.setAttribute('src', 'imagens/mulhercrianca.png')
+    } else if(idade > 14 && idade <= 21 && genero == 'Mulher') {
+        img.setAttribute('src', 'imagens/mulherjovem.png') 
+    } else if(idade > 21 && idade < 60 && genero == 'Mulher') {
+        img.setAttribute('src', 'imagens/mulheradulta.png')
+    } else if (idade >= 60 && genero == 'Mulher') {
+        img.setAttribute('src', 'imagens/mulheridosa.png')
+    }
+}
+if(cpnasc.value >= 1900 && cpnasc.value <= anoatual) {
+    msg.style.textAlign = 'center'
+    msg.innerHTML = `Detectamos ${genero} com ${idade} anos.` 
+    img.style.display = 'block'
+    img.style.margin = 'auto'
+    msg.appendChild(img)
+ }
 }
