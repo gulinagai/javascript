7c573ebb9ab94f8c13c445b63b537fca0c3e004a
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-11-12 17:51:05 -0300

    atualização

--- aula14ex/ex017sozinho/script/script.js
+++ aula14ex/ex017sozinho/script/script.js
@@ -1,17 +1,21 @@
 function gerar(){
 var tabcp = window.document.getElementById('tab')
 var tabnb = Number(tabcp.value)
+var list = window.document.getElementById('list')
 if(tabcp.value.length == 0){
     window.alert('Por favor, digite um número.')
 } else {
+    list.innerHTML = ''
     for(c = 0; c <= 10; c++) {
         var mult = tabnb * c
+        var op = window.document.createElement('option')
+        list.appendChild(op)
         window.document.getElementsByTagName('option')[c].innerHTML += `${tabnb} x ${c} = ${mult}`
     }
 }
 }
 function redefinir() {
     for(c = 0; c <= 10; c++) {
-        window.document.getElementsByTagName('option')[c].innerHTML += ` `
+        window.document.getElementsByTagName('option')[c].innerHTML += ''
     }
 }
