6968a0d23f5724e572811f6286ae34881fec1eaa
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-11-15 14:09:09 -0300

    atualização dos ex de sintaxe de js

--- curso02/arrays-e-loops/script.js
+++ curso02/arrays-e-loops/script.js
@@ -1,12 +1,54 @@
-//plataformajogos.pop()
-//plataformajogos.push('Super Nintendo')
-//var ultimoitem = plataformajogos.pop()
-//console.log(plataformajogos.length)
-var plataformajogos = ['PlayStation 5', 'Nintendo Switch', 'PC', 'GameCube', 'GameBoy Advanced']
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+var brasilGanhouCopa = [1959, 1962, 1970, 1994, 2002]
 
-for(c = 0; c < plataformajogos.length; c++){
-    console.log(plataformajogos[c])
-    if(plataformajogos[c] == 'PC'){
+// Interaja com o array utilizando um loop, para mostrar
+// no console a seguinte mensagem, 'O brasil ganhou a copa de ${ano}'
+for(c = 0; c < brasilGanhouCopa.length ; c++){
+    console.log(`O Brasil ganhou a copa de ${brasilGanhouCopa[c]}`)
+}
+
+//ou com forEach
+
+brasilGanhouCopa.forEach(function(ano){
+    console.log(`O Brasil ganhou a copa de ${ano}`)
+})
+
+//Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melancia']
+
+for(i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+    if(frutas[i] === 'Pera'){
         break
     }
 }
+
+//ou com forEach
+
+// frutas.forEach(function(fruta) {
+//     console.log(fruta)
+//     if(fruta === 'Pera'){
+//         break
+//     }
+// })
+// break no if em forEach não funciona!
+
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array
+var ultimafrutaforma1
+ frutas.forEach(function(fruta){
+     ultimafrutaforma1 = fruta
+    
+ })
+ console.log(ultimafrutaforma1)
+
+for(a = 0; a < frutas.length ; a++) {
+    var ultimafrutaforma2 = frutas[a]
+}
+console.log(ultimafrutaforma2)
+//outra forma
+
+var ultimafrutaforma3 = frutas[frutas.length-1]
+console.log(ultimafrutaforma3)
---
+++ curso02/atribuicao-e-ternario/script.js
@@ -0,0 +1,13 @@
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000
+scroll += 500
+console.log(scroll)
+
+// Atribua true para a variável darCrédito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true
+var possuiCasas = true
+var darCredito = (possuiCarro && possuiCasas) ? 'Dar crédito ao cliente' : 'Negar crédito'
+console.log(darCredito)
