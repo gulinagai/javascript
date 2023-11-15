a15c5e037a1f6f503bd4fc4bbd45b1d4220b5955
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-11-15 00:30:19 -0300

    tudo é objeto js

---
+++ curso02/tudo-e-objeto/script.js
@@ -0,0 +1,20 @@
+// nomeie 3 propriedades ou métodos de strings
+var nome = 'Gustavo'
+nome.length
+nome.replace('vo', 'vin')
+// nomeie 5 proprieddades ou métodos de elementos do DOM
+document.innerHTML = 'olá'
+document.querySelector('.corpo')
+window.document.getElementsByClassName('.corpo')
+var corpo = document.querySelector('.corpo')
+corpo.className
+corpo.addEventListener('click', function() {
+    console.log('Clicou')
+})
+window.document.getElementsByTagName('p')[0].innerHTML = 'tchau'
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard.
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
+
+window.document.
