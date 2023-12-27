a48e444c2769365756a7b05f4a13acb9b78ef58d
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2023-12-27 16:30:12 -0300

    api history, evento popstaste, .pushState()

--- curso02/javascript-assincrono/history-api/script.js
+++ curso02/javascript-assincrono/history-api/script.js
@@ -1,1 +1,38 @@
-console.log(window.history)
+// esse código serve para não precisar dar refresh toda vez que clicar em outra página, criar uma navegação mais fluida!
+const links = document.querySelectorAll('a')
+
+function handleClick(event){
+    // não dá refresh pq to prevenindo o padrão :) e pegando o conteúdo da outro arquivo .html que eu criei e colocando em uma mesma página
+    event.preventDefault()
+    console.log(event.target.href)
+    fetchPage(event.target.href)
+    // console.log('clique')
+    window.history.pushState(null, null, event.target.href)
+}
+
+async function fetchPage(url){
+    urlResponse = await fetch(url)
+    urlText = await urlResponse.text()
+    // console.log(urlText)
+    replaceContent(urlText)
+}
+
+function replaceContent(newText){
+    const newHtml = document.createElement('div')
+    newHtml.innerHTML = newText
+    console.log(newHtml)
+
+    const oldContent = document.querySelector('.content') 
+    const newContent = newHtml.querySelector('.content')
+
+    oldContent.innerHTML = newContent.innerHTML
+    document.title = newHtml.querySelector('title').innerText
+}
+
+window.addEventListener('popstate', ()=>{
+    fetchPage(window.location.href)
+})
+
+links.forEach((link)=>{
+    link.addEventListener('click', handleClick)
+})
