e21f137daafc8328f30a3c42e425fb87daaaaa28
Gustavo Nagai Amorim <148737051+gulinagai@users.noreply.github.com>
2024-01-09 22:33:06 -0300

    exercicio destructuring

--- curso02/mais javascript/destructuring/script.js
+++ curso02/mais javascript/destructuring/script.js
@@ -1,74 +1,30 @@
-const pessoa = 
-{
-    nome: 'Gustavo',
-    idade: 24,
-    namorada: 'Hanna'
+// Extraia a backgroundColor, color e margin de btn
+
+const btn = document.querySelector('button')
+const btnStyles = getComputedStyle(btn)
+
+const {backgroundColor, color, margin} = btnStyles
+
+console.log(btnStyles)
+
+console.log(color, backgroundColor, margin)
+
+// Inverta os valores das variáveis abaixo
+let cursoAtivo = 'JavaScript'
+let cursoInativo = 'HTML';
+
+[cursoAtivo, cursoInativo] = [cursoInativo, cursoAtivo]
+
+console.log(cursoAtivo, cursoInativo)
+
+
+// Corrija o erro abaixo
+const cachorro = {
+    nome: 'Bob',
+    raca: 'Labrador',
+    cor: 'Amarelo'
 }
 
-const {nome, idade, namorada, idadeNamorada = 19} = pessoa
+const {cor: bobCor} = cachorro
 
-console.log(idadeNamorada)
-
-const anime = 
-{
-    ação: {
-        espada: ['SAO', 'Nanatsu no Taizai','Bleach'],
-        caderno: 'death note'
-    },
-    triste: ['Shingeki no Kyojin', 'Shigatsu wa Kimi no uso']
-}
-
-const {ação: {espada, caderno}} = anime
-
-console.log(caderno)
-
-// ou mudar a referência do objeto que está sendo atribuido, mudar ''anime'' para anime.ação por exemplo:
-
-// const {espada, caderno} = anime.ação
-
-// console.log(caderno)
-
-// também posso mudar o nome da propriedade:
-
-const game = {
-    pc: 'LoL',
-    ps3: 'Infamous 2',
-    gameboy: 'Pokémon Emerald'
-}
-
-const {pc: computador, ps3, gameboy} = game
-
-console.log(computador)
-
-console.log(game) // não muda o objeto
-
-// destructuring de array:
-
-const frutas = ['Banana', 'Maçã', 'Pêra']
-
-// nesse caso, coloca todos os valores em uma variável
-const [primeiraFruta, segundaFruta, terceiraFruta] = frutas
-
-console.log(segundaFruta)
-
-// ou também, posso criar várias variáveis de uma vez, usando destructuring de array:
-
-const [carboidrato, outroCarboidrato, proteina, micronutrientes] = ['Arroz', 'Feijão', 'Carne', 'Salada']
-
-// criou uma variável para cada valor!
-
-console.log(`Minha refeição preferida é ${carboidrato} com ${outroCarboidrato}, um pedaço de ${proteina} e um pouco de ${micronutrientes}`)
-
-// também é possível desestruturar argumentos que são passados em funções, e que são objetos!
-
-//exemplo, ao invés de:
-
-// function handleKeyup(event){
-//     console.log(event.key)
-// }
-
-function handleKeyup({key}){
-    console.log(key)
-}
-
-document.addEventListener('keyup', handleKeyup)
+console.log(bobCor)
