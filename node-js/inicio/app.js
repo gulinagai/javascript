977b8085142bd131c2b2f31e9ded19aada07ce93
Gustavo Nagai Amorim <gulifiree@gmail.com>
2024-10-27 18:09:48 -0300

    primeiro código node.js

---
+++ node-js/inicio/app.js
@@ -0,0 +1,32 @@
+var mostrarSite = true
+const site = 'www.guiadoprogramador.com'
+
+console.log('Hello World!')
+console.log('Meu nome é Gustavo Nagai')
+console.log('E eu estou aprendendo node.js com o Guia do Programador')
+
+if(mostrarSite) {  // se mostrar site
+    console.log(site)  // printe o site
+}
+
+
+// COMO EXECUTAR CÓDIGO JAVASCRIPT NO NODE.JS?
+
+// posso usar o cmd do windows mesmo:
+
+// 1º abro o cmd
+// 2º vou até a pasta onde está o meu arquivo .js que eu quero executar
+// por exemplo, ao abrir o cmd estava:
+
+// C:Users\gulif\
+// ai é só digitar:
+
+// cd OneDrive\Desktop\programacao\javascript\node-js\inicio
+
+// ( nesse caso, o OneDrive está ali pq meu pc está vinculado ao OneDrive, mas não necessariamente ele precisa estar )
+
+// 3º agora é só executar, para executar em node.js, basta digitar: node nomedoapp.js
+
+// ex: OneDrive\Desktop\programacao\javascript\node-js\inicio node app.js
+
+// pronto!
---
+++ node-js/oque-e-node-js.js
@@ -0,0 +1,12 @@
+// Node.js é um ambiente de execução que permite utilizar a linguagem JavaScript fora dos navegadores
+// usando o interpretador V8 como mecanismo de execução.
+
+// O V8 é um tipo de interpretador de javascript.
+
+// Interpretador é um software que interpreta e executa código JavaScript. Os navegadores incluem interpretadores
+// (como o V8 no Google Chrome) que permitem rodar JavaScript no navegador.
+
+// Ou seja:
+
+// Interpretador de JavaScript no Google Chrome: V8
+// Ambiente de execução para JavaScript fora de navegadores: Node.js (utilizando o V8 para interpretar o código)
